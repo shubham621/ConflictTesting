35e39e25a400007d329b9a4b68af455743e899c0
shukumar <shukumar@shukumar-rlvcg.1499.lowes.com>
2020-02-05 20:24:11 +0530

    commit-3

--- ConflitTest/src/Print.java
+++ ConflitTest/src/Print.java
@@ -2,8 +2,8 @@
 public class Print {
 
 	public static void main(String[] args) {
-		for(int p=0;p<100;p++)
-			System.out.println("print: "+p);
+		for(int r=0;r<100;r++)
+			System.out.println("print: "+r);
 			
 
 	}
